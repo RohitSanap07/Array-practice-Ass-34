d75299337ee4bfc10e27a3cb74040643d733232e
RohitSanap07 <rohitsanap177@gmail.com>
2024-07-16 14:03:22 +0530

    code completed

--- large.js
+++ large.js
@@ -1,3 +1,12 @@
 // Finding the largest number from the given array
 
 const age = [40,48,70,87,56,43,57]
+let largest = age[0]
+for (const num of age)
+{
+    if(num > largest)
+    {
+        largest = num
+    }
+}
+console.log(`Most Senior: ${largest}`)
